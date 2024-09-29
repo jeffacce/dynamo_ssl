af1599bc54fc192fde10eaef2efccbf5bb73387c
jeffacce <zc2357@nyu.edu>
2024-09-29 18:31:33 -0400

    sim kitchen dataset support lazy load for lower RAM usage

--- datasets/sim_kitchen.py
+++ datasets/sim_kitchen.py
@@ -2,46 +2,57 @@ import utils
 import torch
 import numpy as np
 from pathlib import Path
-from torch.utils.data import TensorDataset
 from datasets.core import TrajectoryDataset
 
 
-class SimKitchenTrajectoryDataset(TensorDataset, TrajectoryDataset):
-    def __init__(self, data_directory, onehot_goals=False):
-        data_directory = Path(data_directory)
-        states = torch.from_numpy(np.load(data_directory / "observations_seq.npy"))
-        actions = torch.from_numpy(np.load(data_directory / "actions_seq.npy"))
-        masks = torch.from_numpy(np.load(data_directory / "existence_mask.npy"))
-        goals = torch.load(data_directory / "onehot_goals.pth")
+class SimKitchenTrajectoryDataset(TrajectoryDataset):
+    def __init__(self, data_directory, prefetch=True, onehot_goals=False):
+        self.data_directory = Path(data_directory)
+        states = torch.from_numpy(np.load(self.data_directory / "observations_seq.npy"))
+        actions = torch.from_numpy(np.load(self.data_directory / "actions_seq.npy"))
+        goals = torch.load(self.data_directory / "onehot_goals.pth")
         # The current values are in shape T x N x Dim, move to N x T x Dim
-        states, actions, masks, goals = utils.transpose_batch_timestep(
-            states, actions, masks, goals
+        self.states, self.actions, self.goals = utils.transpose_batch_timestep(
+            states, actions, goals
         )
-        observations = torch.load(data_directory / "observations_seq_img_multiview.pth")
-        self.masks = masks
-        tensors = [observations, actions, masks]
-        if onehot_goals:
-            tensors.append(goals)
-        TensorDataset.__init__(self, *tensors)
-        self.actions = self.tensors[1]
-        self.states = states
+        self.Ts = np.load(self.data_directory / "existence_mask.npy").sum(axis=0).astype(int).tolist()
+        
+        self.prefetch = prefetch
+        if self.prefetch:
+            self.obses = []
+            for i in range(len(self.Ts)):
+                self.obses.append(torch.load(self.data_directory / "obses" / f"{i:03d}.pth"))
+        self.onehot_goals = onehot_goals
 
     def get_seq_length(self, idx):
-        return int(self.masks[idx].sum().item())
+        return self.Ts[idx]
 
     def get_all_actions(self):
         result = []
         # mask out invalid actions
-        for i in range(len(self.masks)):
-            T = int(self.masks[i].sum())
+        for i in range(len(self.Ts)):
+            T = self.Ts[i]
             result.append(self.actions[i, :T, :])
         return torch.cat(result, dim=0)
 
     def get_frames(self, idx, frames):
-        result = [x[idx, frames] for x in self.tensors]
-        result[0] = result[0] / 255.0
-        return result
+        # obs, act, mask / obs, act, mask, goal
+        if self.prefetch:
+            obs = self.obses[idx][frames]
+        else:
+            obs = torch.load(self.data_directory / "obses" / f"{idx:03d}.pth")[frames]
+        obs = obs / 255.0
+        act = self.actions[idx, frames]
+        mask = torch.ones((len(frames)))
+        if self.onehot_goals:
+            goal = self.goals[idx, frames]
+            return obs, act, mask, goal
+        else:
+            return obs, act, mask
 
     def __getitem__(self, idx):
-        T = self.masks[idx].sum().int().item()
+        T = self.Ts[idx]
         return self.get_frames(idx, range(T))
+    
+    def __len__(self):
+        return len(self.Ts)
